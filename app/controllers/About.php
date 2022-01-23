97b85c1e41dad25e480bcef39fd8c017896eaa74
jaenudin0901 <jaenudinj913198@gmail.com>
2022-01-24 04:46:58 +0700

    menambahkan file constants dan style.css

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -8,7 +8,7 @@ class About extends Controller
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
         $data['judul'] = 'About me';
-        $this->view('templates/header');
+        $this->view('templates/header', $data);
         $this->view('about/index', $data);
         $this->view('templates/footer');
 
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,6 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="<?= BASEURL;?>/css/style.css">
     <title>Halaman <?= $data['judul'];?></title>
 </head>
 <body>
+<section class="header">
+
+<h3>FAEYZA</h3>
+<form action="" method="post">
+
+<input type="text"  autofocus   placeholder="Cari disini" id="keyword" autocomplete="off">
+    
+</form>
+</section>
